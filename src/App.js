fb8337a79f3543b52baeb6fdcafca4a994f97750
Alexsandro <alexsandro.oliveira117@gmail.com>
2020-05-10 01:38:03 -0400

    Fixing useEffect problem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from "react";
-
+import {useEffect,useState} from "react";
 import "./styles.css";
+import api from './services/api'
+
 
 function App() {
+  const [repositories,setRepositories] = useState([]);
+
+  useEffect(()=>{
+    api.get('/repositories').then(response =>{
+      setRepositories(response.data);
+    });
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories',{
+      title:`Novo repositorio ${Date.now()}`,
+      url:`www.${Date.now()}.com`,
+      techs:[],
+    });
+
+    setRepositories([...repositories,response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter(repo => repo.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(r => (
+          <li key={r.id}>
+            {r.title}
+            <button onClick={() => handleRemoveRepository(r.id)}>
+              Remover
+            </button>            
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
